5ee0b4905f1aa8ed02925aa9d39f4c7df094fbff
sanjaymagarzph <sanjay.magar@zakipoint.com>
2020-08-24 01:55:31 +0545

    Updates

---
+++ precache-manifest.b808e349e984a1d497a55eec4019a890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e85eb33ecde5c300d0e901ebf2e1d2",
+    "url": "/alan-ai/index.html"
+  },
+  {
+    "revision": "2e3d553401c95b00270b",
+    "url": "/alan-ai/static/css/main.dddf91e4.chunk.css"
+  },
+  {
+    "revision": "4a823dcc583b92b1e5a2",
+    "url": "/alan-ai/static/js/2.a700b97e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan-ai/static/js/2.a700b97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3d553401c95b00270b",
+    "url": "/alan-ai/static/js/main.03058a88.chunk.js"
+  },
+  {
+    "revision": "adbec9d197cabd71a084",
+    "url": "/alan-ai/static/js/runtime-main.db372d02.js"
+  }
+]);
